b05f6a836a1c6860b1f81be5e27cb16841e25095
Taikatou <conorstephens@gmail.com>
2015-05-24 22:39:49 +0100

    UL app for higher/grad diploma students limit is now changeable

--- app/src/main/java/com/example/ul/shoppinglist/SettingsActivity.java
+++ app/src/main/java/com/example/ul/shoppinglist/SettingsActivity.java
@@ -3,8 +3,6 @@ package com.example.ul.shoppinglist;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.NumberPicker;
 
@@ -22,7 +20,6 @@ public class SettingsActivity extends Activity {
         np = (NumberPicker) findViewById(R.id.numberPicker);
         np.setMaxValue(120);
         np.setMinValue(5);
-        np.setValue(20);
 
     }
 
@@ -41,29 +38,6 @@ public class SettingsActivity extends Activity {
         Intent returnActivity = new Intent(this, MainActivity.class);
         returnActivity.putExtra("amount", "" + np.getValue());
         startActivity(returnActivity);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
 
     }
 }
